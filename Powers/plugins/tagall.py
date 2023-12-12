4e0b4ef67a00edeecdda2f16076a63a2ba2c5517
sparrow9616 <108956698+sparrow9616@users.noreply.github.com>
2023-12-12 08:30:54 +0530

    Update tagall.py

--- Powers/plugins/tagall.py
+++ Powers/plugins/tagall.py
@@ -4,6 +4,7 @@ from Powers.bot_class import Gojo
 from pyrogram import Client, filters
 from pyrogram.errors import UserNotParticipant
 from pyrogram.types import ChatPermissions
+from pyrogram.enums import ChatMemberStatus, ChatType
 
 spam_chats = []
 
@@ -146,7 +147,7 @@ async def mentionall(client, message):
     except UserNotParticipant:
         is_admin = False
     else:
-        if participant.status in ("administrator", "creator"):
+        if participant.status == ChatMemberStatus.ADMINISTRATOR || participant.status == ChatMemberStatus.OWNER:
             is_admin = True
     if not is_admin:
         return await message.reply("𝐘𝐨𝐮 𝐀𝐫𝐞 𝐍𝐨𝐭 𝐀𝐝𝐦𝐢𝐧 𝐁𝐚𝐛𝐲, 𝐎𝐧𝐥𝐲 𝐀𝐝𝐦𝐢𝐧𝐬 𝐂𝐚𝐧 𝐓𝐚𝐠 𝐌𝐞𝐦𝐛𝐞𝐫𝐬. ")
@@ -199,7 +200,7 @@ async def cancel_spam(client, message):
     except UserNotParticipant:
         is_admin = False
     else:
-        if participant.status in ("administrator", "creator"):
+        if participant.status == ChatMemberStatus.ADMINISTRATOR || participant.status == ChatMemberStatus.OWNER:
             is_admin = True
     if not is_admin:
         return await message.reply("𝐘𝐨𝐮 𝐀𝐫𝐞 𝐍𝐨𝐭 𝐀𝐝𝐦𝐢𝐧 𝐁𝐚𝐛𝐲, 𝐎𝐧𝐥𝐲 𝐀𝐝𝐦𝐢𝐧𝐬 𝐂𝐚𝐧 𝐓𝐚𝐠 𝐌𝐞𝐦𝐛𝐞𝐫𝐬.")
